0f4c649ea565c254c1bfb7a286f9a19d5c72f157
xcxerxes <15807237048@163.com>
2016-11-27 22:44:44 +0800

    包含time,date,selector的picker选择器

---
+++ src/containers/App.js
@@ -0,0 +1,13 @@
+import React,{Component} from 'react'
+import Picker from './Picker'
+
+
+export default class App extends Component{
+    render(){
+        return(
+            <div className="app">
+                <Picker />
+            </div>
+        )
+    }
+}
